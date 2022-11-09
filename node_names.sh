7d7fc5703be959532ae12f06de2e4d86ed548501
konstantin petrenko <vizarblr@gmail.com>
2022-11-09 13:24:08 +0100

    chore(monitoring): improve script with inserting node names to edgeapplication chart

--- node_names.sh
+++ node_names.sh
@@ -8,10 +8,13 @@ array=($nodes)
 array=("${array[@]/#/  - name: }")
 touch node.yaml
 
+yq 'del(.targetNodeGroups.[])' -i ./charts/node-exporter/values.yaml 
+yq 'del(.targetNodeGroups.[])' -i ./charts/grafana-agent/values.yaml
+
 for str in ${array[@]}; do
   echo $str >> node.yaml
 done
 
-sed -i -e '/name: virtual-*/d' ./charts/node-exporter/values.yaml ./charts/grafana-agent/values.yaml
+sed -i -e 's/.*targetNodeGroups:.*/targetNodeGroups:/' ./charts/node-exporter/values.yaml ./charts/grafana-agent/values.yaml
 sed -i -e '/targetNodeGroups:/r node.yaml' ./charts/node-exporter/values.yaml ./charts/grafana-agent/values.yaml
 rm -f node.yaml
